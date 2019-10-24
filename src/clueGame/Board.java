af9acd18b8e455b3fde73ef08ff139476015fb1d
Chase Patterson <cpatterson@mines.edu>
2019-10-24 12:13:07 -0600

    Moved the logic of calcAdjacencies to another method

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -35,8 +35,7 @@ public class Board {
 		this.types = new HashSet<String>();
 		this.grid = new BoardCell[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
 	}
-	
-	
+
 	// Calculates adjacency list for each grid cell and stores into map adjStore
 	public void calcAdjacencies() {
 		// loop through grid
@@ -44,81 +43,85 @@ public class Board {
 			for (int j = 0; j < numCols; j++) {
 				Set<BoardCell> tempSet = new HashSet<BoardCell>();
 				char boardChar = grid[i][j].getInitial();
-
-				if (boardChar == 'W') {
-					// Checks above, below, left and right if it is a valid space add it to our temp
-					// set of valid spaces
-					BoardCell current = grid[i][j];
-					if (i - 1 >= 0) {
-						// Checks UP
-						BoardCell up = grid[i - 1][j];
-						if (up.getInitial() == 'W' || (up.isDoorway() && up.getDoorDirection() == DoorDirection.DOWN)) {
-							tempSet.add(up);
-						}
-					}
-					if (i + 1 < numRows) {
-						// Checks DOWN
-						BoardCell down = grid[i + 1][j];
-						if (down.getInitial() == 'W'
-								|| (down.isDoorway() && down.getDoorDirection() == DoorDirection.UP)) {
-							tempSet.add(down);
-						}
-					}
-					if (j - 1 >= 0) {
-						// Checks LEFT
-						BoardCell left = grid[i][j - 1];
-						if (left.getInitial() == 'W'
-								|| (left.isDoorway() && left.getDoorDirection() == DoorDirection.RIGHT)) {
-							tempSet.add(left);
-						}
-					}
-					if (j + 1 < numCols) {
-						// Checks RIGHT
-						BoardCell right = grid[i][j + 1];
-						if (right.getInitial() == 'W'
-								|| (right.isDoorway() && right.getDoorDirection() == DoorDirection.LEFT)) {
-							tempSet.add(right);
-						}
-					}
-					// update the valid spaces list from our temp set
-					adjStore.put(current, tempSet);
-				} else {
-					BoardCell current2 = grid[i][j];
-					// if it is a room the temp set should be null
-					if (current2.isRoom()) {
-						adjStore.put(current2, tempSet);
-					}
-					if (current2.isDoorway()) {
-						DoorDirection my_door = current2.getDoorDirection();
-						// if the space is a doorway then add the corresponding exit to the set
-						switch (my_door) {
-						case UP:
-							// adds UP
-							BoardCell up = grid[i - 1][j];
-							tempSet.add(up);
-							break;
-						case DOWN:
-							// adds DOWN
-							BoardCell down = grid[i + 1][j];
-							tempSet.add(down);
-							break;
-						case LEFT:
-							// adds LEFT
-							BoardCell left = grid[i][j - 1];
-							tempSet.add(left);
-							break;
-						case RIGHT:
-							// adds RIGHT
-							BoardCell right = grid[i][j + 1];
-							tempSet.add(right);
-							break;
-						default:
-							System.out.println("error in door");
-							break;
-						}
-						adjStore.put(current2, tempSet);
-					}
+				cellCheck(tempSet, boardChar, i, j);
+			}
+		}
+	}
+	
+	public void cellCheck(Set<BoardCell> tempSet, char boardChar, int i, int j) {
+		if (boardChar == 'W') {
+			// Checks above, below, left and right if it is a valid space add it to our temp
+			// set of valid spaces
+			BoardCell current = grid[i][j];
+			if (i - 1 >= 0) {
+				// Checks UP
+				BoardCell up = grid[i - 1][j];
+				if (up.getInitial() == 'W' || (up.isDoorway() && up.getDoorDirection() == DoorDirection.DOWN)) {
+					tempSet.add(up);
 				}
+			}
+			if (i + 1 < numRows) {
+				// Checks DOWN
+				BoardCell down = grid[i + 1][j];
+				if (down.getInitial() == 'W'
+						|| (down.isDoorway() && down.getDoorDirection() == DoorDirection.UP)) {
+					tempSet.add(down);
+				}
+			}
+			if (j - 1 >= 0) {
+				// Checks LEFT
+				BoardCell left = grid[i][j - 1];
+				if (left.getInitial() == 'W'
+						|| (left.isDoorway() && left.getDoorDirection() == DoorDirection.RIGHT)) {
+					tempSet.add(left);
+				}
+			}
+			if (j + 1 < numCols) {
+				// Checks RIGHT
+				BoardCell right = grid[i][j + 1];
+				if (right.getInitial() == 'W'
+						|| (right.isDoorway() && right.getDoorDirection() == DoorDirection.LEFT)) {
+					tempSet.add(right);
+				}
+			}
+			// update the valid spaces list from our temp set
+			adjStore.put(current, tempSet);
+		} 
+		else {
+			BoardCell current2 = grid[i][j];
+			// if it is a room the temp set should be null
+			if (current2.isRoom()) {
+				adjStore.put(current2, tempSet);
+			}
+			if (current2.isDoorway()) {
+				DoorDirection my_door = current2.getDoorDirection();
+				// if the space is a doorway then add the corresponding exit to the set
+				switch (my_door) {
+				case UP:
+					// adds UP
+					BoardCell up = grid[i - 1][j];
+					tempSet.add(up);
+					break;
+				case DOWN:
+					// adds DOWN
+					BoardCell down = grid[i + 1][j];
+					tempSet.add(down);
+					break;
+				case LEFT:
+					// adds LEFT
+					BoardCell left = grid[i][j - 1];
+					tempSet.add(left);
+					break;
+				case RIGHT:
+					// adds RIGHT
+					BoardCell right = grid[i][j + 1];
+					tempSet.add(right);
+					break;
+				default:
+					System.out.println("error in door");
+					break;
+				}
+				adjStore.put(current2, tempSet);
 			}
 		}
 	}
@@ -127,54 +130,56 @@ public class Board {
 	public void calcTargets(int row, int column, int pathLength) {
 		// Resets target list for subsequent calls
 		targets.clear();
-		//Set<BoardCell> visited = new HashSet<BoardCell>();
+		// Set<BoardCell> visited = new HashSet<BoardCell>();
 		BoardCell visited = new BoardCell();
 		BoardCell current = grid[row][column];
 		int pathTraverse = pathLength;
 		pathGen(pathLength, pathTraverse, grid[row][column], visited, current);
 	}
-	
+
 	// Recursively moves through each possible path available to the player
-	void pathGen(int pathLength, int pathTraverse, BoardCell start, 
-			BoardCell visited, BoardCell current) {
-		// Base case, reached at the end of each path. Adds the cell at the end of the path
-		// to the targets set and resets everything else 
+	void pathGen(int pathLength, int pathTraverse, BoardCell start, BoardCell visited, BoardCell current) {
+		// Base case, reached at the end of each path. Adds the cell at the end of the
+		// path
+		// to the targets set and resets everything else
 		if (pathTraverse == 0) {
 			pathTraverse = pathLength;
-			if (current != visited && current != start) targets.add(current);
+			if (current != visited && current != start)
+				targets.add(current);
 			current = start;
 			return;
 		}
-		
+
 		// Recursive case; when there are still unvisited paths, visit them
 		// Move to all unvisited adjacent locations
-		for (BoardCell adjCell: adjStore.get(current)) {
+		for (BoardCell adjCell : adjStore.get(current)) {
 			if (visited != adjCell && adjCell != start) {
 				pathGen(pathLength, pathTraverse - 1, start, current, adjCell);
-				if (adjCell.isDoorway()) targets.add(adjCell);
-		    }
-	    }
+				if (adjCell.isDoorway())
+					targets.add(adjCell);
+			}
+		}
 	}
 
-	// Loads the board information from the input files and the adjacency information
+	// Loads the board information from the input files and the adjacency
+	// information
 	public void initialize() {
 		try {
 			loadRoomConfig();
 			loadBoardConfig();
 			calcAdjacencies();
-		} 
-		catch (FileNotFoundException | BadConfigFormatException e) {
+		} catch (FileNotFoundException | BadConfigFormatException e) {
 			System.out.println(e.getMessage());
 		}
 	}
-	
+
 	// Reads in legend file to a map
 	public void loadRoomConfig() throws FileNotFoundException, BadConfigFormatException {
-		//Initialize
-		FileReader roomIn = new FileReader(roomConfigFile);		
+		// Initialize
+		FileReader roomIn = new FileReader(roomConfigFile);
 		Scanner scanner = new Scanner(roomIn);
 
-		//loop through and parse by ", "
+		// loop through and parse by ", "
 		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine();
 			String arr[] = line.split(", ");
@@ -184,23 +189,23 @@ public class Board {
 			String roomName = arr[1];
 			String type = arr[2];
 
-			//add into legend
+			// add into legend
 			legend.put(symbol, roomName);
 
-			//add into type
+			// add into type
 			types.add(type);
-			
+
 			if (!type.equals("Card") && !type.contentEquals("Other")) {
 				throw new BadConfigFormatException("Error: Unsupported format in file " + roomConfigFile);
 			}
 		}
-		//clean up
+		// clean up
 		scanner.close();
 	}
-	
+
 	// Reads in the board itself from a CSV file
 	public void loadBoardConfig() throws FileNotFoundException, BadConfigFormatException {
-		//Initialize
+		// Initialize
 		FileReader file = new FileReader(boardConfigFile);
 		Scanner dimFinder = new Scanner(file);
 
@@ -209,7 +214,7 @@ public class Board {
 		String arr[] = tempRow.split(",");
 		this.numCols = arr.length;
 		this.numRows++;
-		
+
 		while (dimFinder.hasNextLine()) {
 			tempRow = dimFinder.nextLine();
 			arr = tempRow.split(",");
@@ -221,72 +226,72 @@ public class Board {
 
 		dimFinder.close();
 		
-
-
 		FileReader file2 = new FileReader(boardConfigFile);
 		Scanner scanner = new Scanner(file2);
 
 		BoardCell[][] setterGrid = new BoardCell[numRows][numCols];
 		int count = 0;
 
-		//go until end of file 
+		// go until end of file
 		while (scanner.hasNextLine()) {
-			//read in line and parse into array
+			// read in line and parse into array
 			String line = scanner.nextLine();
 			arr = line.split(",");
 
-			//go through array and add the correct index into the setter grid
+			// go through array and add the correct index into the setter grid
 			for (int i = 0; i < arr.length; i++) {
 				char initial = 0;
 				char door = ' ';
-				
+
 				initial = arr[i].charAt(0);
-				//if there is a case with 2 chars such as KD for kitchen down
+				// if there is a case with 2 chars such as KD for kitchen down
 				if (arr[i].length() == 2) {
 					door = arr[i].charAt(1);
 				}
 				if (!legend.containsKey(initial)) {
 					throw new BadConfigFormatException("Error: Board does not contain key " + initial);
 				}
-				//index correct l and add
+				// index correct l and add
 				BoardCell tempBoardCell = new BoardCell(count, i, initial, door);
 				setterGrid[count][i] = tempBoardCell;
 			}
 			count++;
 		}
-		//set grid and clean up
-		
+		// set grid and clean up
 		grid = setterGrid;
 		scanner.close();
 	}
-	
 
 	// Instance method, makes sure there is only one instance of board
 	// Also provides a global access point to the board
 	public static Board getInstance() {return instance;}
 	// Returns list of targets
 	public Set<BoardCell> getTargets() {return targets;}
-	//Returns row number
+
+	// Returns row number
 	public int getNumRows() {return numRows;}
-	//Returns col number
+
+	// Returns col number
 	public int getNumColumns() {return numCols;}
 
 	// Returns adjacency list for one BoardCell
 	public Set<BoardCell> getAdjList(int row, int col) {
-		if (adjStore.containsKey(grid[row][col])) return adjStore.get(grid[row][col]);
-		else return null;
+		if (adjStore.containsKey(grid[row][col]))
+			return adjStore.get(grid[row][col]);
+		else
+			return null;
 	}
 
 	// Returns a single cell if it's within range, if not, returns null
 	public BoardCell getCellAt(int row, int column) {
 		if (row >= 0 && row < numRows && column >= 0 && column < numCols) {
 			return grid[row][column];
-		}
-		else return null;
+		} else
+			return null;
 	}
 
 	public Map<Character, String> getLegend() {return legend;}
-	
+
 	public void setConfigFiles(String string, String string2) {
 		this.boardConfigFile = string;
 		this.roomConfigFile = string2;
